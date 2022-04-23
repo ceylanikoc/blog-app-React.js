557b4075320efc37e72c055ad05e92a460d88284
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-23 11:40:53 +0300

    State üzerinden blog bilgisi sillme

--- src/index.js
+++ src/index.js
@@ -2,30 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './routers/AppRouter'
 import reportWebVitals from './reportWebVitals';
-import "./App.css"
+import "./App.css";
+import { v4 as uuid } from 'uuid';
+
 
 import { createStore, combineReducers } from 'redux'
 
-const state = {
-    blogs: [
-        {
-            id : 1,
-            title: "blog title 1",
-            description: "blog 1 description",
-            dateAdded : 0
-        }
-    ],
-    auth : {
-        userid: 1,
-        username: "ceylani",
-        email: "info@ceylani.com"
+
+// ACTION CREATOR
+
+const addBlog = ({title='',description='',dateAdded=0}) => ({
+    type: "ADD_BLOG",
+    blog: {
+        id:uuid(),
+        title: title,
+        description: description,
+        dateAdded: dateAdded
     }
-}
+})
+
+const removeBlog = ({ id }) => (
+    {
+        type: "REMOVE_BLOG",
+        id: id
+    }
+)
 
 const blogsState = []
 
 const blogReducer = (state = blogsState,action) => {
     switch (action.type) {
+        case "ADD_BLOG" :
+            return [
+                ...state,
+                action.blog
+            ]
+        case "REMOVE_BLOG" :
+            return state.filter(({ id }) => {
+                return id !== action.id
+            })   
         default :
             return state;
     }
@@ -46,7 +61,17 @@ const store = createStore(combineReducers({
     auth : authReducer
 }));
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+const blog1 = store.dispatch(addBlog({title:"blog title 1", description:"description blog 1"}))
+const blog2 = store.dispatch(addBlog({title:"blog title 2", description:"description blog 2", dateAdded: Date.now()}))
+
+console.log(blog1.blog.id);
+
+
+store.dispatch(removeBlog({id: blog1.blog.id}))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
