c5e01c7bbe9fcd75504e0d1bd6f47e74a516c2e7
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-23 08:51:19 +0300

    Redux add

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import AppRouter from './routers/AppRouter'
 import reportWebVitals from './reportWebVitals';
 import "./App.css"
 
+import { createStore } from 'redux'
+
+
+const initialState = {
+    count : 0
+}
+
+// Store
+
+const store = createStore((state = initialState) => {
+    return state
+})
+
+// getState
+
+console.log(store.getState());
+
+
+// INCREMENT, DECREMENT , RESET => ACTIONS (object)
+
+// DISPATCH
+
+// REDUCERS
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AppRouter />
