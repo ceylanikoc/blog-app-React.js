df909e60e7a2af6db1644ae4f4de58446f346aa5
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-23 09:41:53 +0300

    Dispatch Actions

--- src/index.js
+++ src/index.js
@@ -13,16 +13,49 @@ const initialState = {
 
 // Store
 
-const store = createStore((state = initialState) => {
-    return state
+const store = createStore((state = initialState,action) => {
+    console.log('store created');
+    switch (action.type) {
+        case  "INCREMENT" :
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count + incrementBy
+            }
+        case "DECREMENT" : 
+            return {
+                count: state.count -1
+            }
+        case "RESET" : 
+            return {
+                count: 0
+            }    
+        default :
+            return state        
+    }
 })
 
 // getState
 
 console.log(store.getState());
 
+store.subscribe(() => {
+    console.log(store.getState() + 'store üzerinde değişiklik olduğundan çalışır sadece');
+})
 
 // INCREMENT, DECREMENT , RESET => ACTIONS (object)
+
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy : 10
+})
+
+store.dispatch({
+    type: "INCREMENT"
+})
+
+console.log(store.getState());
+
+
 
 // DISPATCH
 
