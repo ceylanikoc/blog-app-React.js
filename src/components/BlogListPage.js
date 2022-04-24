2d6fd8a105c7ddeae8d15fd5e3cd8a742aee279c
Ceylani Koç <42927448+ceylanikoc@users.noreply.github.com>
2022-04-24 15:08:26 +0300

    Blog Detay Sayfası Hazırlandı

--- src/components/BlogListPage.js
+++ src/components/BlogListPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import BlogList from './BlogList'
 
 function BlogListPage() {
@@ -7,8 +6,6 @@ function BlogListPage() {
     <div>
         <p>Blog List Page</p>
         <BlogList />
-        <Link to="/blogs/1">Blog 1</Link>
-        <Link to="/blogs/2">Blog 2</Link>
     </div>
   )
 }
